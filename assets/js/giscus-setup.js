7c05e6295877a3f20e6dd08b9c2ff8c08e888799
estellerogers197-art <estellerogers197@gmail.com>
2025-10-30 11:53:49 +0000

    Deploying to gh-pages from @ estellerogers197-art/leihuang.github.io@4e32b465fc99422decd3aa039c821db11873246d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "estellerogers197-art/polycazuza.github.io",
+    "data-repo": "estellerogers197-art/leihuang.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
